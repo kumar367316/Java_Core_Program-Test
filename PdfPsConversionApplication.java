8b0b4abe58a38ec26a1f7bd07b1527c2004ce6ce
kumar <85148531+kumar367316@users.noreply.github.com>
2023-09-28 16:29:17 +0530

    Add files via upload

---
+++ PdfPsConversionApplication.java
@@ -0,0 +1,13 @@
+package com.pdf.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfPsConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfPsConversionApplication.class, args);
+	}
+
+}
